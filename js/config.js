03cae3442ec40500c891525cb7857a37df723f18
Mrmoistv2 <56588598+Mrmoistv2@users.noreply.github.com>
2019-10-17 19:35:19 -0600

    Update config.js

--- js/config.js
+++ js/config.js
@@ -10,7 +10,7 @@
 // Leave it blank to use text instead
 // (Put the files in the images folder)
 // (You can also use external URL)
-var l_serverImage = "logo.png";
+var l_serverImage = "QUANTUM-RPTransparent (1).png";
 
 // Center logo?
 // 'true' or 'false'
